bf0b2751bfb204d79044f36c2857b8a32b0cffae
Hans Hübner <hans.huebner@gmail.com>
2011-04-05 19:59:48 +0200

    add socket.io test service for tetra frontent

--- run-webserver.js
+++ run-webserver.js
@@ -4,8 +4,10 @@
  * reachable from the directory where node is running.
  */
 var fs = require('fs'),
+sys = require('sys'),
 antinode = require('./antinode/lib/antinode'),
-sys = require('sys');
+_ = require('underscore'),
+io = require('socket.io'),
 bcr2000 = require('./src/bcr2000-web.js');
 
 var settings = {
@@ -17,4 +19,24 @@ var settings = {
     }
 };
 
-antinode.start(settings);
+var server = antinode.start(settings);
+var socket = io.listen(server);
+
+function newSocketClient(client) {
+    console.log('new socket client');
+    process.stdin.resume();
+    process.stdin.on('data', function (data) {
+        data = data.toString('ascii', 0, data.length - 1);
+        console.log('stdin data', data);
+        client.send(data);
+    });
+    client.on('message', function (message) {
+        console.log('client message', message);
+    });
+    client.on('disconnect', function () {
+        console.log('client disconnect');
+        process.stdin.removeAllListeners();
+    });
+}
+
+socket.on('connection', newSocketClient);
