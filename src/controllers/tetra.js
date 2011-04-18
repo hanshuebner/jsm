855a9a1edc9ced22fe5c7e463696361d35d96a22
Hans Hübner <hans.huebner@gmail.com>
2011-04-18 17:17:42 +0200

    load tetra preset from file

--- src/controllers/tetra.js
+++ src/controllers/tetra.js
@@ -1,3 +1,5 @@
+
+var fs = require('fs');
 var MIDI = require('MIDI');
 var tetraDefs = require('tetra-defs');
 
@@ -21,7 +23,7 @@ function decodePackedMSB(packed)
 {
     var retval = [];
     var highBits;
-    for (var i in packed) {
+    for (var i = 0; i < packed.length; i++) {
         if ((i % 8) == 0) {
             highBits = packed[i];
         } else {
@@ -37,6 +39,54 @@ function decodePackedMSB(packed)
 function toHexString (array, length) {
     return array.slice(0, length).map(function (x) { return x.toString(16) }).join(' ');
 }
+
+Buffer.prototype.toHexString = function (start, end) {
+    start = start || 0;
+    end = (end == undefined) ? this.length : end;
+    if (end > this.length) {
+        throw Error('end beyond buffer length');
+    }
+    var retval = '';
+    for (var i = start; i < end; i++) {
+        if (retval) {
+            retval += ' ';
+        }
+        retval += this[i].toString(16);
+    }
+    return retval;
+}
+
+function readSysexDump(filename, callback)
+{
+    var presets = [];
+    function processSysexMessage(buf) {
+        switch (buf.toHexString(0, 4)) {
+        case tetraSysexResponse.programDataDump:
+            var parameters = decodePackedMSB(buf.slice(6, buf.length - 1));
+            var programName = (new Buffer(parameters.slice(184, 200), 'binary')).toString();
+            presets.push({ name: programName,
+                           bank: buf[4],
+                           program: buf[5],
+                           parameters: parameters });
+        }
+    }
+
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            callback(err);
+        }
+        var messageStart = 0;
+        for (var i = 0; i < data.length; i++) {
+            if (data[i] == 0xf7) {
+                processSysexMessage(data.slice(messageStart, i + 1));
+                messageStart = i + 1;
+            }
+        }
+        callback(null, presets);
+    });
+}
+
+exports.readSysexDump = readSysexDump;
 
 exports.make = function(_hub) {
     hub = _hub;
