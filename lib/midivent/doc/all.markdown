fc628c351ffc0e63afb93d95a00b475001704ac8
Hans Huebner <hans.huebner@gmail.com>
2011-04-26 23:34:55 +0200

    remove local midivent copy again

--- lib/midivent/doc/all.markdown
+++
@@ -1,5 +0,0 @@
-@include synopsis
-@include utils
-@include input
-@include output
-
--- lib/midivent/doc/makedoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for markdown in *.markdown
-do
-    html=html/`basename $markdown .markdown`.html
-    echo $markdown '=>' $html
-    node ../doctool/doctool.js template.html $markdown > $html
-done
