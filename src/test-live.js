7f860a2ef87de81841acdbc48d589a4e5fd8a08b
Hans Huebner <hans.huebner@gmail.com>
2011-06-09 20:49:37 +0200

    new live osc api changes

--- src/test-live.js
+++ src/test-live.js
@@ -1,12 +1,12 @@
 var Ableton = require('./live.js');
-var live = new Ableton.Live();
+var live = new Ableton.Live('localhost');
 
 live.on('/live/tempo', function (bpm) { console.log('bpm', bpm); });
 live.on('/live/name/clip', function (track, clip, name, color) { console.log('clip', track + '/' + clip, 'changed name to', name) });
 
 live.send('/live/tempo', 120);
 live.send('/live/tempo', 121);
-live.send('/remix/reconfigure', '', 9001);
+live.send('/remix/set_peer', '', 9001);
 live.send('/remix/echo', 'hello');
 live.send('/remix/set_peer', '192.168.5.1', 9090);
 live.send('/remix/echo', 'nix is');
